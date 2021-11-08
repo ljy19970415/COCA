91c8bebe76e8b982bb19a0e6427ae58bd5c6e99b
ljy19970415 <thunderbey@163.com>
2021-11-08 17:16:57 +0800

    Update annotate.py

--- annotate.py
+++ annotate.py
@@ -9,9 +9,9 @@ class Annotate():
     def __init__(self, dataset, e, a, rootpath, net):
         self.e = e
         self.a = a
-        self.q = e//a # 计算utility时表示业余者可以标注的次数
-        # self.classReward = 0 # 具体大小取决于|\delta H|
-        # self.classTruth={} # 记录字符串标签的id
+        self.q = e//a 
+        # self.classReward = 0 
+        # self.classTruth={}
         self.rootpath=rootpath
         self.dataset=dataset
         self.readDataset()
@@ -23,40 +23,38 @@ class Annotate():
         self.halflabel = {}
         self.discoveredClass = []
         self.error = 1
-        self.correct = 0.1
-        self.batch_size=64
-        self.e_num=0 # 专家标记的个数
-        self.a_num=0 # 业余者标记的个数
-        self.a_correct=0 # 业余者指出正确类别的次数
-        self.a_fp=0 # 将错误的标签贴到样本上
-        self.a_tp=0 # 将正确的标签贴到样本上
-        self.a_fn=0 #将正确的样本排除
-        self.a_tn=0 #将错误的样本排除
+        self.correct = 0.5
+        self.e_num=0
+        self.a_num=0
+        self.a_correct=0
+        self.a_fp=0
+        self.a_tp=0 
+        self.a_fn=0 
+        self.a_tn=0 
         
-        self.lastAccuracy=0 #上一次的业余者识别精度
+        self.lastAccuracy=0
         self.myCluster=Cluster(self.l, net, dataset)
-        self.reallabelDic={} # 记录每个类别下有哪些已标注样本
+        self.reallabelDic={}
 
         self.prev=len(self.discoveredClass)
         self.no_progress=0
         self.tail_flag=False
 
-        self.ae_ratio=15
+        self.max_ae_ratio = 4
+        self.min_ae_ratio = 1
+        self.ae_ratio=self.min_ae_ratio
+        self.batch_size= 64
     
+    def genAccuracy(self):
+        mu,sigma=0.9,0.03
+        s=np.random.normal(mu,sigma,1)
+        return s
+
     def readDataset(self):
-        # 设置self.classTruth
-        # with open(os.path.join('/datapool/workspace/leijiayu/MAAS/'+dataset+'/classes.txt'), 'r') as f:
-        #     while True:
-        #         line = f.readline()
-        #         if not line:
-        #             break
-        #         class_id, class_name = [i for i in line.split()]
-        #         self.classTruth[int(class_id)-1] = class_name
-        # 设置self.label
-        self.label = np.load(self.dataset+'labels.npy')
-        # 设置self.n
+        self.label = np.load('Datasets/'+self.dataset+'labels.npy')
         self.n = len(self.label)
         self.l = len(set(self.label))
+        print("total catagories: "+str(self.l))
     
     def continue_from_middle(self):
         print("begin")
@@ -82,108 +80,85 @@ class Annotate():
         np.save(self.rootpath+'middle_point/reallabelDic.npy', self.reallabelDic)
         np.save(self.rootpath+'middle_point/k.npy',[k])
 
-    def annotate(self,k, isContinue, eSelect, aNum, aSelect, net, p):
-        # eSelect指专家在类别发现之后的选择策略, 1表示有筛选随机, 2表示自定义策略
-        # aNum指业余者在每个batch中标注数目, 1表示batchSize*accuracy_last_time, 2表示固定大小
-        # aSelect指业余者数据选择策略, 1表示各类别选择一张, 2表示第一个类别选择aNum张
-        # p表示业余者正确率
+    def annotate(self,k, isContinue, eSelect, aSelect, net):
         if isContinue:
             self.continue_from_middle()
         if k==1: 
-            self.firstBatch(self.dataset+net,p)
+            self.firstBatch(self.dataset+net)
         else:
-            self.restBatch(eSelect, aNum, aSelect,p)
+            self.restBatch(eSelect, aSelect)
         if self.left<=0:
             return False
-        if len(self.reallabel)==self.n: # 若成本还没有用完就已经标完全部数据集
+        if len(self.reallabel)==self.n:
             return False
         self.saveMiddle(k)
         return True
     
-    def firstBatch(self, path, p):
+    def firstBatch(self, path):
         first, second = self.myCluster.firstCluster(path)
         self.expertAnnotate(first)
         correct=0
+        temp=set()
         for clas,idx in enumerate(second):
-            # clas为聚类标号，idx为应标注的label
-            if idx in first:
+            if idx in first or idx in temp:
                 continue
+            else:
+                temp.add(idx)
             self.a_num += 1
             self.left -= self.a
-            # 加噪后判断业余者是否返回正确答案
-            reallabel_flag=False
-            noise_answer=random.random()
+            isCorrect=1
             if self.label[idx]==self.label[first[clas]]:
-                if noise_answer<=p:
-                    reallabel_flag=True
-                    self.a_correct += 1
-                    self.a_tp+=1
-                else:
-                    self.a_fn+=1
-            else:
-                if noise_answer>p:
-                    reallbel_flag=True
-                    self.a_fp+=1
-                else:
-                    self.a_tn+=1
-            if reallabel_flag:
+                self.a_correct += 1
+                self.a_tp+=1
                 correct+=1
-                self.reallabelDic[self.annotation[first[clas]]].append(idx)
+                self.reallabelDic[self.annotation[first[clas]]].append((idx,isCorrect,1))
                 self.reallabel.append(idx)
                 self.annotation[idx] = self.annotation[first[clas]]
             else:
+                self.a_tn+=1
                 self.halflabel[idx]=[self.annotation[first[clas]]]
-            #print(idx)
             self.unlabel.remove(idx)
-        # 设置上一轮已发现类别个数prev
-        # 设置持续未发现新类别的轮数no_progress
-        # 设置长尾现象标志为False
         self.prev=len(self.discoveredClass)
         self.no_progress=0
         self.tail_flag=False
-        # 统计上一轮业余者正确率
         self.lastAccuracy=correct/(len(second))
 
-    def restBatch(self, eSelect, aNum, aSelect, p):
+    def restBatch(self, eSelect, aSelect):
         self.updateModel()
-        a_annotate=self.genAmateurAnnotate(aNum, aSelect)
-        if p<1.0:
-            self.amateurNoiseAnnotate(a_annotate,p)
-        else:
-            self.amateurAnnotate(a_annotate)
-        e_annotate=self.genExpertAnnotate(a_annotate, eSelect) # 专家在本batch中不重复标amateur标过的图片
+        a_annotate=self.genAmateurAnnotate(aSelect)
+        self.amateurAnnotate(a_annotate)
+        e_annotate=self.genExpertAnnotate(a_annotate, eSelect)
         self.expertAnnotate(e_annotate)
-        # if p<1.0:
-        #     self.amateurNoiseAnnotate(a_annotate,p)
-        # else:
-        #     self.amateurAnnotate(a_annotate)
-        # 判断类别发现是否开始有长尾现象
         if len(self.discoveredClass)<self.l:
             self.isTail()
             print('no progress:'+str(self.no_progress)+'; isTail:'+str(self.tail_flag))
-
-    def genAmateurAnnotate(self, aNum, aSelect):
-        # 找到每个已知类别应该标注的样本，顺便计算门槛值，返回amateur标注的id列表
+    
+    def genAmateurAnnotate(self, aSelect):
         a_idlist=[]
         # num=int(np.ceil(self.batch_size*self.lastAccuracy)) if int(np.ceil(self.batch_size*self.lastAccuracy))!=0 else (10 if self.n-len(self.reallabel)>10 else 0)
-        num=min((self.batch_size-int(np.ceil(self.batch_size*self.lastAccuracy)))*self.ae_ratio,len(self.unlabel)+len(self.halflabel))
-        #num=min(self.batch_size,len(self.unlabel)+len(self.halflabel))
+        #print("expert_num:"+str(n2)+"; punish:"+str(n1)+"; ae_ratio:"+str(self.ae_ratio)+"; anum:"+str(n3))
+        num=min(np.ceil((self.batch_size-int(np.ceil(self.batch_size*self.lastAccuracy)))*self.ae_ratio),len(self.unlabel)+len(self.halflabel))
         if num==0:
             return a_idlist
         #num=min(num,len(self.myCluster.amateur_rank))
+        if aSelect==1:
+            # random
+            legal=list(self.halflabel.keys())+self.unlabel
+            return random.sample(legal,num) if num<len(legal) else legal
+        if aSelect==2:
+            # min entropy
+            self.myCluster.entropy.sort(key=lambda x:-x[0])
+            start=max(0,len(self.myCluster.entropy)-num)
+            return [i[1] for i in self.myCluster.entropy[start:]]
+        if aSelect==3:
+            # max confidence
+            self.myCluster.maxconfi.sort(key=lambda x:-x[0])
+            end=min(len(self.myCluster.maxconfi)-1,num)
+            return [i[1] for i in self.myCluster.maxconfi[:end]]
         count=0
         rank_margin = num % len(self.myCluster.amateur_rank)
-        if aSelect==1:
-            # 每个类别抽一张
-            # for label in self.myCluster.amateur_rank:
-            #     # 或许存在有的类别没有样本在其上置信度最大的情况，所以要判断classification[label]是否为空
-            #     if len(self.myCluster.classification[label]):
-            #         self.myCluster.classification[label] = sorted(self.myCluster.classification[label], key=lambda x:abs(x[0]-0.5))
-            #         a_idlist.append(self.myCluster.classification[label][0][1])
-            #         count+=1
-            #         if count==num:
-            #             break
-            # 每个类别抽多张
+        if aSelect==4: 
+            # COCA
             class_num=0
             for idx,label in enumerate(self.myCluster.amateur_rank):
                 if len(self.myCluster.classification[label]):
@@ -227,48 +202,27 @@ class Annotate():
             self.tail_flag=False
 
     def genExpertAnnotate(self, a_annotate, eSelect):
-        # 剔除门槛值大于相应threshold的点，在剩余点中随机选择expert应该标的点，返回expert应该标注的id列表
         legal=[]
         print("lastAccuracy:"+str(self.lastAccuracy))
         print("batchsize:"+str(self.batch_size))
         num=self.batch_size-int(np.ceil(self.batch_size*self.lastAccuracy))
         print("expert_num:"+str(num))
         # num=int(self.batch_size-len(a_annotate))
-        # 若是纯随机情况，或者类别未发现完全且为长尾现象
-        if eSelect==6 or (self.tail_flag and len(self.discoveredClass)<self.l):
+        # random
+        if eSelect==1 or (self.tail_flag and len(self.discoveredClass)<self.l):
             print("6 or class discover random select")
             # legal=list(set(list(self.halflabel.keys())+self.unlabel)-set(a_annotate))
             legal=list(self.halflabel.keys())+self.unlabel
             return random.sample(legal, num) if num<len(legal) else legal
-        # 若类别已发现完全
+        # all categories discovered
         if len(self.discoveredClass)==self.l: # 若类别发现结束
-            if eSelect==2: # 方差大的类别依次选择
-                count=0
-                for label in self.myCluster.expert_rank:
-                    threshold=self.myCluster.classification[label][0].confidence if len(self.myCluster.classification[label]) else 0.5
-                    for item in self.myCluster.classification[label][::-1]:
-                        if item[0]<=threshold and item[1] not in a_annotate:
-                            legal.append(item[0])
-                            count+=1
-                            if count==num:
-                                break
-                    if count==num:
-                        break
-                return legal
-            if eSelect==3: # 从样本最少的类别标起
-                count=0
-                for label in self.myCluster.num_rank:
-                    if len(self.myCluster.classification[label]):
-                        threshold=self.myCluster.classification[label][0][0]
-                        for item in self.myCluster.classification[label]:
-                            if item[0]<=threshold and item[1] not in a_annotate:
-                                legal.append(item[1])
-                                count+=1
-                                break
-                    if count==num:
-                        break
-                return legal
-            if eSelect==4: # 方差大的类别一个选一张
+            if eSelect==2: # max entropy
+                end=min(len(self.myCluster.entropy)-1,num)
+                return [i[1] for i in self.myCluster.entropy[:end]]
+            if eSelect==3: # least confidence
+                start=max(0,len(self.myCluster.maxconfi)-num)
+                return [i[1] for i in self.myCluster.maxconfi[start:]]
+            if eSelect==4: # COCA
                 print("done discover, expert select")
                 count=0
                 for label in self.myCluster.expert_rank:
@@ -292,17 +246,7 @@ class Annotate():
                     legal2=random.sample(candidate, num-count) if len(candidate)>num-count else candidate
                     return legal+legal2
                 return legal
-            if eSelect==5: # 剩下所有的样本熵降序排列
-                count=0
-                entrophy=sorted(self.myCluster.entrophy, key=lambda x:-x[0])
-                for item in entrophy:
-                    if item[1] not in a_annotate:
-                        legal.append(item[1])
-                        count+=1
-                        if count==num:
-                            break
-                return legal
-        # 若类别未发现完全但不为长尾现象
+        # category discovery
         print("class_discover, filter random select")
         for i in range(self.n):
             #if i in self.reallabel or i in a_annotate:
@@ -316,51 +260,8 @@ class Annotate():
         return random.sample(legal, num) if num<len(legal) else legal
 
     def updateModel(self):
-        self.myCluster.restCluster(self.reallabelDic, self.halflabel, self.reallabel, self.unlabel)
+        self.myCluster.restCluster(self.reallabelDic, self.halflabel, self.reallabel)
         self.myCluster.rankClass(self.reallabelDic)
-    
-    def amateurNoiseAnnotate(self, idlist, p):
-        correct=0
-        for idx in idlist:
-            self.left -= self.a
-            self.a_num += 1
-            i = np.argmax(self.myCluster.confidence[idx])
-            noise_answer=random.random()
-            reallabel_flag=False
-            if self.label[idx]==self.discoveredClass[i]:
-                if noise_answer<=p:
-                    reallabel_flag=True
-                    self.a_correct += 1
-                    self.a_tp+=1
-                else:
-                    self.a_fn+=1
-            else:
-                if noise_answer>p:
-                    reallabel_flag=True
-                    self.a_fp+=1
-                else:
-                    self.a_tn+=1
-            if reallabel_flag:
-                correct += 1
-                self.reallabel.append(idx)
-                self.reallabelDic[i].append(idx)
-                self.annotation[idx]=i
-                if idx in self.halflabel:
-                    del self.halflabel[idx]
-                else:
-                    self.unlabel.remove(idx)
-            else:
-                if idx in self.halflabel:
-                    self.halflabel[idx].append(i)
-                else:
-                    self.halflabel[idx]=[i]
-                    self.unlabel.remove(idx)
-        now_Accuracy=correct/len(idlist) if len(idlist)!=0 else 0
-        if self.lastAccuracy<=now_Accuracy:
-            self.ae_ratio=max(15,self.ae_ratio+1)
-        else:
-            self.ae_ratio=min(10,self.ae_ratio-1)
-        self.lastAccuracy= now_Accuracy
 
     def amateurAnnotate(self, idlist):
         correct=0
@@ -384,7 +285,7 @@ class Annotate():
                 self.a_correct += 1
                 correct += 1
                 self.reallabel.append(idx)
-                self.reallabelDic[i].append(idx)
+                self.reallabelDic[i].append((idx,1,1))
                 self.annotation[idx]=i
                 if i==pre_label:
                     class_correct+=1
@@ -400,23 +301,27 @@ class Annotate():
                     self.unlabel.remove(idx)
         now_Accuracy=correct/len(idlist) if len(idlist)!=0 else 0
         if self.lastAccuracy<=now_Accuracy:
-            self.ae_ratio=max(15,self.ae_ratio+1)
+            self.ae_ratio=min(self.max_ae_ratio,self.ae_ratio+1)
         else:
-            self.ae_ratio=min(10,self.ae_ratio-1)
+            self.ae_ratio=max(self.min_ae_ratio,self.ae_ratio-1)
         self.lastAccuracy= now_Accuracy
-        res1=np.argsort(res)
 
     def expertAnnotate(self, idlist):
+        temp=set()
         for idx in idlist:
+            if idx in temp or idx in self.reallabel:
+                continue
+            else:
+                temp.add(idx)
             self.left -= self.e
             self.e_num += 1
             if len(self.discoveredClass)<self.l and self.label[idx] not in self.discoveredClass:
                 self.discoveredClass.append(self.label[idx])
-                self.reallabelDic[len(self.discoveredClass)-1]=[idx]
+                self.reallabelDic[len(self.discoveredClass)-1]=[(idx,1,0)]
                 self.reallabel.append(idx)
             else:
                 i = self.discoveredClass.index(self.label[idx])
-                self.reallabelDic[i].append(idx)
+                self.reallabelDic[i].append((idx,1,0))
                 self.reallabel.append(idx)
             if idx in self.halflabel:
                 del self.halflabel[idx]
@@ -462,6 +367,3 @@ class Annotate():
                 a += 1
         min_num, max_num, std = self.isClassEven()
         return a/self.n, self.B-self.left, len(self.discoveredClass), self.e_num, self.a_num, self.a_correct, min_num, max_num, std
-
-if __name__=='__main__':
-    print("i am new")
